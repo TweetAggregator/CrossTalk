8fb1e612cb3fa02bd45bec9ad7b75ffabd439136
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-03-17 09:43:46 +0100

    Example of dummy test for the application. Addition of the libs for twitter4j (perhaps not needed). Please enter the commit message for your changes. Lines starting

---
+++ test/DummyTest.scala
@@ -0,0 +1,19 @@
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+import play.api.test._
+import play.api.test.Helpers._
+import akka.actor.ActorSystem
+import akka.actor.Props
+import jobs.DummyActor
+
+@RunWith(classOf[JUnitRunner])
+class DummyTest extends Specification {
+
+  "Dummy Actor" should {
+    "Print Hello world" in new WithApplication {
+      val dummyActor = ActorSystem().actorOf(Props(new DummyActor()))
+      dummyActor ! "Hello World !"
+    }
+  }
+}
