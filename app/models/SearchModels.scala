34c6a673f28e55dd1035b4f4cf480d1ff47ef99a
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-03-27 21:48:14 +0100

    Query model: addition of a date for the SEARCH
    Since the SEARCH API will look into the past, addition of a maxAge
    field into the SearchQuery keyword, which will be used by the SEARCH
    API to determine if a tweet is too old to be considered.

--- app/models/SearchModels.scala
+++ app/models/SearchModels.scala
@@ -1,23 +1,24 @@
 package models
 
 import play.api.libs.json.JsValue
+import java.util.Date
 
-/** 
- *  Model of a geographic position. The parameters passed are ready to be used by the Streaming API. 
- *  For the Search API, the parameters need to be converted into a central position + radius, hence
- *  the calculus in the inner fields.
+/**
+ * Model of a geographic position. The parameters passed are ready to be used by the Streaming API.
+ * For the Search API, the parameters need to be converted into a central position + radius, hence
+ * the calculus in the inner fields.
  */
-case class GeographicPos(lat1:Double, long1: Double, lat2: Double, long2: Double) {
+case class GeographicPos(lat1: Double, long1: Double, lat2: Double, long2: Double) {
   val center: (Double, Double) = ??? /* TODO */
   val radius: String = ??? /* TODO */
 }
 
-/** 
- *  Model of a research launcher, based on some keywords filters.
+/**
+ * Model of a research launcher, based on some keywords filters.
  */
-case class SearchQuery(keywords: List[String], area: GeographicPos, rows: Int, cols: Int) {
+case class SearchQuery(keywords: List[String], area: GeographicPos, rows: Int, cols: Int, maxAge: Date) {
   val subqueries: List[SearchQuery] = (rows, cols) match {
-    case (1,1) => this :: Nil /* Since we cannot split it anymore */
+    case (1, 1) => this :: Nil /* Since we cannot split it anymore */
     case _ => ??? /* TODO: split the query into smaller search queries */
   }
 }
