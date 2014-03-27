359d1c5aff9e538f4971324ec1a49966971361f0
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-03-27 21:42:33 +0100

    Tentative of coding Case Class for search models
    Following our discussion, simple addition of the search model we could
    use to communicate with the research with the Twitters APIs, both the
    Streaming API and the Search API, which could be launch simultaneously.

---
+++ app/models/SearchModels.scala
@@ -0,0 +1,29 @@
+package models
+
+import play.api.libs.json.JsValue
+
+/** 
+ *  Model of a geographic position. The parameters passed are ready to be used by the Streaming API. 
+ *  For the Search API, the parameters need to be converted into a central position + radius, hence
+ *  the calculus in the inner fields.
+ */
+case class GeographicPos(lat1:Double, long1: Double, lat2: Double, long2: Double) {
+  val center: (Double, Double) = ??? /* TODO */
+  val radius: String = ??? /* TODO */
+}
+
+/** 
+ *  Model of a research launcher, based on some keywords filters.
+ */
+case class SearchQuery(keywords: List[String], area: GeographicPos, rows: Int, cols: Int) {
+  val subqueries: List[SearchQuery] = (rows, cols) match {
+    case (1,1) => this :: Nil /* Since we cannot split it anymore */
+    case _ => ??? /* TODO: split the query into smaller search queries */
+  }
+}
+
+/**
+ * Model a returned matching tweet from a query, along with its geographic position. Since tweets
+ * are JSON Value, we return a JsValue, i.e. the parsed JSON code ready to be used.
+ */
+case class Tweet(value: JsValue, area: GeographicPos)
