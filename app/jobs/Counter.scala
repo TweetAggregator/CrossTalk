8a2e3feb7a546a30b313be45e3a24b2efa16bf28
Lewis Brown <lewix7@hotmail.com>
2014-03-31 11:00:33 +0200

    Add a counter actor
    This counter is designed to count Tweets as they are received from the
    TweetSearcher. It notifies a listener every time its counter is updated.

---
+++ app/jobs/Counter.scala
@@ -0,0 +1,22 @@
+package jobs
+
+import akka.actor.Actor
+import akka.actor.ActorRef
+
+import models._
+
+class Counter(listener: ActorRef) extends Actor {
+  var count = 0L
+
+  def increase(n: Int) = {
+    count += n
+    listener ! n
+  }
+
+  def get() = count
+
+  def receive = {
+    case ts: Seq[_] => increase(ts.size)
+    case _ => increase(1)
+  }
+}
