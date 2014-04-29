1d427bf88671777ad687277091ecfb665c0b2f6f
Lewis Brown <lewix7@hotmail.com>
2014-04-29 17:10:49 +0200

    Skeleton for the GatheringController
    And for the GatheringContoller test.  Currently has four actions, the return
    values will probably be changed at some point.

--- app/controllers/Gathering.scala
+++ app/controllers/Gathering.scala
@@ -8,6 +8,31 @@ import play.api.data.Forms._
 /**
  * Gathering the tweets and processing the results
  */
-object Gathering extends Controller {
+trait GatheringController { this: Controller =>
+  def start = {
+    //TODO: get form data from cache
+    //TODO: start a tweetmanager searching for the data
+    //TODO: get translations
+    Ok
+  }
+
+  def pause = {
+    //TODO: stop the tweetmanager
+    Ok
+  }
+
+  def resume = {
+    //TODO: start the tweetmanager again
+    Ok
+  }
+
+  def computeDisplayData = {
+    //TODO: opacity
+    //TODO: Venn diagram
+    //TODO: clustering
+    Ok
+  }
 
 }
+
+object Gathering extends GatheringController with Controller
---
+++ test/GatheringSpec.scala
@@ -0,0 +1,8 @@
+import org.specs2.mutable._
+import org.specs2.runner._
+
+object GatheringControllerSpec extends Specification {
+
+  class TestController() extends Controller with GatheringController
+
+}
