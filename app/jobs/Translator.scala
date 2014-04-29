fe97b528c0db704e0f4e7d8798773f45db9fe949
Lewis Brown <lewix7@hotmail.com>
2014-04-29 19:06:52 +0200

    Fix some bugs in translator
    Need to lowercase the keywords and catch exceptions in case there are no results.

--- app/jobs/Translator.scala
+++ app/jobs/Translator.scala
@@ -1,5 +1,8 @@
+package jobs
+
 import utils.Http._
 import play.api.libs.json.Json
+import play.api.libs.json.JsResultException
 import play.api.libs.json.Reads
 import play.api.libs.json.JsValue
 import play.api.libs.json.JsObject
@@ -11,15 +14,20 @@ import play.api.libs.json.JsUndefined
 // class Translator(from: String, destL: List[String], keyword: String => (List[List[JsValue]], List[JsValue]) ) {
 case class Translator(from: String, destL: List[String], keyword: String){
 	def apply(): (List[List[JsValue]], List[JsValue]) = {
-		( translate(from, destL, keyword), synonyms(from, keyword) )
+		( translate(from, destL, keyword.toLowerCase), synonyms(from, keyword.toLowerCase) )
 		// translate(from, destL, keyword)
 	}
 
 	/** Parse a string into a list of JsValue  containing translation */
 	def parseJson(str: String): List[JsValue] = {
-	val json = Json.parse(str.toString)
-	(json \ "tuc").as[JsArray].value.map(x => (x \ "phrase" \ "text") ).filter(json => !json.isInstanceOf[JsUndefined]).toList
+	  try { 
+      val json = Json.parse(str.toString)
+      (json \ "tuc").as[JsArray].value.map(x => (x \ "phrase" \ "text") ).filter(json => !json.isInstanceOf[JsUndefined]).toList
+    } catch {
+      case e: JsResultException => List()
+    }
 	}
+
 
 	def translate(from: String, dest: String, keyword: String): List[JsValue] = {
 	  val serverAddr = "http://glosbe.com/gapi/translate?from=" + from + "&dest=" + dest + "&format=json&phrase=" + keyword + "&pretty=true&tm=false"
