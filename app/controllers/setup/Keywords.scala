238fef4280eee5a0f68700ffb8fbddc2b7df3bd8
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-05-03 17:34:03 +0200

    Form for keywords + design
    - Correction of the JQuery for the forms
    - Correction of the design (wrong Jquery import, in footer moreover).
    - Addition of some CSS styles.

--- app/controllers/setup/Keywords.scala
+++ app/controllers/setup/Keywords.scala
@@ -11,62 +11,49 @@ import views._
 import models._
 
 object Keywords extends Controller {
-  
+
   /**
    * Contact Form definition.
    */
-  val contactForm: Form[AllTranslations] = Form(
-    
-    // Defines a mapping that will handle Contact values
+  val keywordsForm: Form[AllTranslations] = Form(
     mapping(
-      // Defines a repeated mapping
       "translations" -> seq(
         mapping(
-          "language" -> optional(email),
-          "keywords" -> list(
-            text) 
-        )(Translation.apply)(Translation.unapply)
-      )
-      
-    )(AllTranslations.apply)(AllTranslations.unapply)
-  )
-  
+          "language" -> optional(text),
+          "keywords" -> list(text))(Translation.apply)(Translation.unapply)))(AllTranslations.apply)(AllTranslations.unapply))
+
   /**
    * Display an empty form.
    */
   def form = Action {
-    Ok(html.form(contactForm));
+    Ok(html.keywordsForm(keywordsForm));
   }
-  
+
   /**
    * Display a form pre-filled with an existing Contact.
    */
   def finalSubmission = Action { implicit request =>
-val resultForm = contactForm.bindFromRequest.get
+    val resultForm = keywordsForm.bindFromRequest.get
 
-
-println("THIS IS THE END " + resultForm)
-Ok(html.end())
+    println("THIS IS THE END " + resultForm)
+    Ok(html.keywordsEnd())
   }
 
-  
   /**
    * Handle form submission.
    */
   def initialSubmission = Action { implicit request =>
 
-    println("MAMAN : " + request)
-val resultForm = contactForm.bindFromRequest.get
-println(resultForm)
-println("RESULT :D:D:D:D " + resultForm.translations.apply(0).keywords)
+    val resultForm = keywordsForm.bindFromRequest.get
+    println(resultForm)
+    //println("RESULT :D:D:D:D " + resultForm.translations.apply(0).keywords)
 
-val keywords = resultForm.translations.apply(0).keywords
+    val keywords = resultForm.translations.apply(0).keywords
 
     val startLanguage = "en"
-    val targetLanguages = List("fra","de")
+    val targetLanguages = List("fra", "de")
 
-
-/*    val tradsAndSyns =
+    /*    val tradsAndSyns =
       for (keyword <- keywords) yield {
         val (trads, syns) = routes.Translator(startLanguage, targetLanguages, keyword)()
         (trads.flatten ++ syns).map(_.as[String])
@@ -75,16 +62,12 @@ val keywords = resultForm.translations.apply(0).keywords
     //renvoyer l anglais en premier
     //les autres langues sont optionnelles
     val existingContact = AllTranslations(
-     translations = List(
+      translations = List(
         Translation(
-         Some("Afficher l anglais en premier toujours!"), keywords
-        ),
+          Some("Afficher l anglais en premier toujours!"), keywords),
         Translation(
-         Some("Français"), List("mouton", "belier")
-        )
-      )
-    )
-    Ok(html.summary(contactForm.fill(existingContact)))
-    
+          Some("Français"), List("mouton", "belier"))))
+    Ok(html.keywordsSummary(keywordsForm.fill(existingContact)))
+
   }
 }
