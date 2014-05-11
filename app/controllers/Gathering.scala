7e0ca22922b9daea03db8cbc13a668eb5eb27eee
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-05-11 13:05:10 +0200

    Fixbug in Gathering Controller
    - Stop, resume, start, pause should handle errors
    - Fix bug in granularity (should not return 0...)

--- app/controllers/Gathering.scala
+++ app/controllers/Gathering.scala
@@ -40,15 +40,16 @@ trait GatheringController { this: Controller =>
   implicit def defaultTimeout: Timeout = 4 seconds
 
   type Square = (Double, Double, Double, Double)
-  // For each square, three geoparts: keyword1, keyword2 and keywor1&2
+  
+  /* For each square, three geoparts: keyword1, keyword2 and keywor1&2 */
   val geoParts: MutableMap[Square, (ActorRef, ActorRef, ActorRef)] = MutableMap()
   var keys: Option[(String, String)] = None
   val maxGranularity = 20
-  val minSide = 20.0 //TODO: choose a reasonable min side
+  val minSide = 0.5 //TODO: choose a reasonable min side
 
   def granularity(top: Double, bottom: Double): Int = {
     require(top > bottom)
-    val highest: Int = ((top - bottom) / minSide).toInt
+    val highest: Int = ((top - bottom) / minSide).toInt + 1
     if (highest > maxGranularity) maxGranularity
     else highest
   }
@@ -66,79 +67,85 @@ trait GatheringController { this: Controller =>
   }
 
   def start() = Action {
-    //TODO: error handling
-    //  - Don't start twice
-    println("Gathering_Start_Action")
+    if (!Cache.get("isStarted").isDefined) {
 
-    //TODO remove those cache writes
-    Cache.set("coordinates", List((-129.4, 20.0, -79.0, 50.6)))
-    Cache.set("keywords",
-      List(("Obama", List[String]()),
-        ("Beer", List("biere", "pression"))))
-    Cache.set("focussed", (-109.4, 40.0, -79.0, 50.6))
+      //TODO remove those cache writes
+      /*Cache.set("coordinates", List((-129.4, 20.0, -79.0, 50.6)))
+      Cache.set("keywords",
+        List(("Obama", List[String]()),
+          ("Beer", List("biere", "pression"))))
+      Cache.set("focussed", (-109.4, 40.0, -79.0, 50.6))*/
 
-    val squaresOption = Cache.getAs[List[Square]]("coordinates")
-    println(squaresOption)
-    // Keywords and translations: list of tuple (initialKeyword, translations&synonyms)
-    val keywordsListOption = Cache.getAs[List[(String, List[String])]]("keywords")
-    println(keywordsListOption)
+      val squaresOption = Cache.getAs[List[Square]]("coordinates")
+      // Keywords and translations: list of tuple (initialKeyword, translations&synonyms)
+      val keywordsListOption = Cache.getAs[List[(String, List[String])]]("keywords")
 
-    (squaresOption, keywordsListOption, keys) match {
-      case (Some(squares), Some((k1, trs1) :: (k2, trs2) :: Nil), None) =>
-        try {
-          keys = Some((k1, k2))
-          val finished: MutableList[Future[_]] = MutableList()
-          val keys1 = k1 :: trs1
-          val keys2 = k2 :: trs2
-          var keys3 = for (key1 <- keys1; key2 <- keys2) yield (s"${key1} ${key2}") // Space means AND, concantenation means OR
-          for (square <- squares) {
-            val gps = (geoPart(square, keys1),
-              geoPart(square, keys2),
-              geoPart(square, keys3))
-            finished += gps._1 ? StartGeo
-            finished += gps._2 ? StartGeo
-            finished += gps._3 ? StartGeo
-            geoParts += square -> (gps._1, gps._2, gps._3)
+      (squaresOption, keywordsListOption, keys) match {
+        case (Some(squares), Some((k1, trs1) :: (k2, trs2) :: Nil), None) =>
+          try {
+            keys = Some((k1, k2))
+            val finished: MutableList[Future[_]] = MutableList()
+            val keys1 = k1 :: trs1
+            val keys2 = k2 :: trs2
+            var keys3 = for (key1 <- keys1; key2 <- keys2) yield (s"${key1} ${key2}") // Space means AND, concantenation means OR
+            for (square <- squares) {
+              val gps = (geoPart(square, keys1),
+                geoPart(square, keys2),
+                geoPart(square, keys3))
+              finished += gps._1 ? StartGeo
+              finished += gps._2 ? StartGeo
+              finished += gps._3 ? StartGeo
+              geoParts += square -> (gps._1, gps._2, gps._3)
+            }
+
+            finished.foreach(Await.ready(_, 10 seconds))
+
+            tweetManagerRef ! Start
+            Cache.set("isStarted", true)
+            Cache.set("isRunning", true)
+            Redirect(routes.Gathering.controlDisplay)
+          } catch {
+            case e: TimeoutException => println("Gathering_Start_InternalServerError"); InternalServerError
           }
-
-          finished.foreach(Await.ready(_, 10 seconds))
-
-          tweetManagerRef ! Start
-          Cache.set("isStarted", true)
-          Cache.set("isRunning", true)
-          Redirect(routes.Gathering.controlDisplay)
-        } catch {
-          case e: TimeoutException => println("Gathering_Start_InternalServerError"); InternalServerError
-        }
-      case _ => println("Gathering_Start_BadRequest"); BadRequest
-    }
+        case t => println("Gathering_Start_BadRequest\n" + t); BadRequest
+      }
+    } else Redirect(routes.Application.index)
   }
 
   def pause = Action {
-    tweetManagerRef ! Pause
-    Cache.remove("isRunning")
+    if (Cache.get("isRunning").isDefined) {
+      tweetManagerRef ! Pause
+      Cache.remove("isRunning")
+    }
     Redirect(routes.Gathering.controlDisplay)
   }
 
   def resume = Action {
-    tweetManagerRef ! Resume
-    Cache.set("isRunning", true)
+    if (!Cache.get("isRunning").isDefined) {
+      tweetManagerRef ! Resume
+      Cache.set("isRunning", true)
+    }
     Redirect(routes.Gathering.controlDisplay)
   }
 
   def stop = Action {
-    tweetManagerRef ! Stop
-    geoParts.empty
-    Cache.remove("isStarted")
+    if (Cache.get("isStarted").isDefined) {
+      tweetManagerRef ! Stop
+      geoParts.clear()
+      keys = None
+      Cache.remove("isStarted")
+      Cache.remove("isRunning")
+      Cache.remove("coordinates")
+      Cache.remove("focussed")
+      Cache.remove("keywords")
+    }
     Redirect(routes.Application.index)
   }
 
   def controlDisplay = Action {
-    var sum = 0L
     val allGeos = geoParts.flatMap(v => v._2._1 :: v._2._2 :: v._2._3 :: Nil)
-    allGeos foreach (_! Collect)
-    sum += askGeos[Long](allGeos, TotalTweets).sum
-    Ok(views.html.gathering(sum))
+    allGeos foreach (_ ! Collect)
+    Ok(views.html.gathering(askGeos[Long](allGeos, TotalTweets).sum))
   }
 
   def computeDisplayData = Action {
--- app/jobs/Counter.scala
+++ app/jobs/Counter.scala
@@ -10,20 +10,14 @@ class Counter(pos: GeoSquare, listener: ActorRef) extends Actor {
   var count = 0L
   val file = new PrintWriter(new File("tweets/counter" + pos.##.toString + ".txt"))
 
-  def increase(n: Int) = {
-    count += n
-   // listener ! (id, n)
-  }
-
-  def get() = count
-  /*TODO change this so that report is a default Object*/
   def receive = {
-    case ts: Seq[_] => increase(ts.size)
+    case ts: Seq[_] => count + ts.size
     case ReportCount =>
       listener ! Report(pos, count)
+      count = 0L /* reset the counter to avoid reporting twice */
     case t => 
       file.write(t.toString + "\n")
       file.flush()
-      increase(1)
+      count += 1
   }
 }
